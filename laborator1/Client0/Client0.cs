478d5723f96a216a871722fc11229146cc050a2f
Alexandra <alexandrataranu2002@gmail.com>
2024-02-12 14:54:10 +0200

    Adaugare laborator 1

---
+++ laborator1/Client0/Client0.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    static void Main()
+    {
+        IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+        int serverPort = 9000;
+
+        Thread receiveThread = new Thread(() => ReceiveMessages(serverIP, serverPort));
+        receiveThread.Start();
+
+        while (true)
+        {
+            Console.WriteLine("Enter text to send to the server (or type 'exit' to quit): ");
+            //?? provide a default value
+            string text = Console.ReadLine() ?? "";
+
+            if (text.ToLower() == "exit")
+                break;
+
+            SendMessage(serverIP, serverPort, text);
+        }
+    }
+
+    static void SendMessage(IPAddress serverIP, int serverPort, string message)
+    {
+        try
+        {
+            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                client.Connect(new IPEndPoint(serverIP, serverPort));
+                Console.WriteLine("Connected to the server");
+
+                byte[] messageData = Encoding.UTF8.GetBytes(message);
+                client.Send(messageData);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error sending message: {ex.Message}");
+        }
+    }
+
+    static void ReceiveMessages(IPAddress serverIP, int serverPort)
+    {
+        try
+        {
+            while (true)
+            {
+                using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    client.Connect(new IPEndPoint(serverIP, serverPort));
+                    Console.WriteLine("Connected to the server for receiving messages");
+
+                    string receivedText = "";
+
+                    do
+                    {
+                        byte[] receivedBuffer = new byte[1024];
+                        int bytesReceived = client.Receive(receivedBuffer);
+                        receivedText += Encoding.UTF8.GetString(receivedBuffer, 0, bytesReceived);
+                    } while (client.Available > 0);
+
+                    Console.WriteLine($"Server: {receivedText}\n");
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error receiving messages: {ex.Message}");
+        }
+    }
+}
+
+
+
---
+++ laborator1/Server/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    static void Main()
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        serverSocket.Bind(new IPEndPoint(ipAddress, 9000));
+        serverSocket.Listen(5);
+
+        while (true)
+        {
+            Socket clientSocket = serverSocket.Accept();
+            Console.WriteLine($"Connection accepted from port {clientSocket.RemoteEndPoint}");
+
+            // separate thread to handle each client
+            Thread clientThread = new Thread(() => HandleClient(clientSocket));
+            clientThread.Start();
+        }
+    }
+
+    static void HandleClient(Socket clientSocket)
+    {
+        try
+        {
+            string text = "";
+
+            do
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = clientSocket.Receive(buffer);
+                text += Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            } while (clientSocket.Available > 0);
+
+            Console.WriteLine($"Received from {clientSocket.RemoteEndPoint}: {text}");
+
+            byte[] response = Encoding.UTF8.GetBytes("Server received your message");
+            clientSocket.Send(response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error handling client: {ex.Message}");
+        }
+        finally
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+}
+
