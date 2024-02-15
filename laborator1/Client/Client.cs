5fb6086f1ed10506df1efceb22a2d4a4cdf41ae5
Alexandra <alexandrataranu2002@gmail.com>
2024-02-15 16:26:33 +0200

    Code changed

--- laborator1/Client/Client.cs
+++ laborator1/Client/Client.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,70 +6,70 @@ using System.Threading;
 
 class Client
 {
+    static string userName="";
+
     static void Main()
     {
+        Console.WriteLine("Enter your username: ");
+        userName = Console.ReadLine();
+
         IPAddress serverIP = IPAddress.Parse("127.0.0.1");
         int serverPort = 9000;
 
-        Thread receiveThread = new Thread(() => ReceiveMessages(serverIP, serverPort));
-        receiveThread.Start();
-
-        while (true)
+        using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
         {
-            Console.WriteLine("Enter text to send to the server (or type 'exit' to quit): ");
-            //?? provide a default value
-            string text = Console.ReadLine() ?? ""; 
+            client.Connect(new IPEndPoint(serverIP, serverPort));
+            Console.WriteLine($"Connected to the server as '{userName}'");
 
-            if (text.ToLower() == "exit")
-                break;
+            Thread receiveThread = new Thread(() => ReceiveMessages(client));
+            receiveThread.Start();
 
-            SendMessage(serverIP, serverPort, text);
+            while (true)
+            {
+                Console.WriteLine("Enter text to send to the server (or type 'exit' to quit): ");
+                string text = Console.ReadLine() ?? "";
+
+                if (text.ToLower() == "exit")
+                    break;
+
+                SendMessage(client, text);
+            }
         }
     }
 
-    static void SendMessage(IPAddress serverIP, int serverPort, string message)
+    static void SendMessage(Socket client, string message)
     {
         try
         {
-            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-            {
-                client.Connect(new IPEndPoint(serverIP, serverPort));
-                Console.WriteLine("Connected to the server");
-
-                byte[] messageData = Encoding.UTF8.GetBytes(message);
-                client.Send(messageData);
-            }
+            string fullMessage = $"{userName}: {message}";
+            byte[] messageData = Encoding.UTF8.GetBytes(fullMessage);
+            client.Send(messageData);
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error sending message: {ex.Message}");
         }
     }
-
-    static void ReceiveMessages(IPAddress serverIP, int serverPort)
+    static void ReceiveMessages(Socket client)
     {
         try
         {
-            while (true)
+            StringBuilder receivedTextBuilder = new StringBuilder();
+            byte[] receivedBuffer = new byte[1024];
+
+            while (client.Connected && client.Receive(receivedBuffer) > 0)
             {
-                using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-                {
-                    client.Connect(new IPEndPoint(serverIP, serverPort));
-                    Console.WriteLine("Connected to the server for receiving messages");
+                receivedTextBuilder.Append(Encoding.UTF8.GetString(receivedBuffer, 0, receivedBuffer.Length));
+            }
 
-                    string receivedText = "";
-
-                    do
-                    {
-                        byte[] receivedBuffer = new byte[1024];
-                        int bytesReceived = client.Receive(receivedBuffer);
-                        receivedText += Encoding.UTF8.GetString(receivedBuffer, 0, bytesReceived);
-                    } while (client.Available > 0);
-
-                    Console.WriteLine($"Server: {receivedText}\n");
-                }
-
-                Thread.Sleep(1000);
+            string receivedText = receivedTextBuilder.ToString();
+            if (!string.IsNullOrEmpty(receivedText))
+            {
+                Console.WriteLine(receivedText);
+            }
+            else
+            {
+                Console.WriteLine("Connection closed by the server.");
             }
         }
         catch (Exception ex)
@@ -79,6 +78,4 @@ class Client
         }
     }
 }
-
-
 
--- laborator1/Server/Server.cs
+++ laborator1/Server/Server.cs
@@ -13,12 +13,13 @@ class Server
         serverSocket.Bind(new IPEndPoint(ipAddress, 9000));
         serverSocket.Listen(5);
 
+        Console.WriteLine("Waiting for a connection...");
+
         while (true)
         {
             Socket clientSocket = serverSocket.Accept();
-            Console.WriteLine($"Connection accepted from port {clientSocket.RemoteEndPoint}");
+            Console.WriteLine($"Connection accepted from {clientSocket.RemoteEndPoint}");
 
-            // separate thread to handle each client
             Thread clientThread = new Thread(() => HandleClient(clientSocket));
             clientThread.Start();
         }
@@ -28,29 +29,28 @@ class Server
     {
         try
         {
-            string text = "";
+            string userName = $"User {clientSocket.RemoteEndPoint}";
 
-            do
+            while (true)
             {
-                byte[] buffer = new byte[1024];
-                int bytesRead = clientSocket.Receive(buffer);
-                text += Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            } while (clientSocket.Available > 0);
+                string receivedText = "";
 
-            Console.WriteLine($"Received from {clientSocket.RemoteEndPoint}: {text}");
+                do
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = clientSocket.Receive(buffer);
+                    receivedText += Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                } while (clientSocket.Available > 0);
 
-            byte[] response = Encoding.UTF8.GetBytes("Server received your message");
-            clientSocket.Send(response);
+                Console.WriteLine($"{userName} - {receivedText}");
+
+                byte[] response = Encoding.UTF8.GetBytes("Server received your message");
+                clientSocket.Send(response);
+            }
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error handling client: {ex.Message}");
         }
-        finally
-        {
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
-        }
     }
 }
-
